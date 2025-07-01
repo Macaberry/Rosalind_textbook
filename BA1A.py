f0f5ff371223e0e897a1dd294906a2fab10dec64
Jiho Park <sei012003@gmail.com>
2025-07-01 04:34:57 +0000

    1

--- BA1A.py
+++ BA1A.py
@@ -2,8 +2,8 @@
 
 def PatternCount(text, pattern):
     count = 0
-    for i in range(len(text) - len(pattern)):
-        if text[i:i+len(pattern)-1] == pattern:
+    for i in range(len(text) - len(pattern) +1):
+        if text[i:i+len(pattern)] == pattern:
             count += 1
     return count
 
