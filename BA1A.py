30e4025d84b4a64a9317d14cf23e4e8c6fcbc72d
Jiho Park <sei012003@gmail.com>
2025-07-01 04:29:33 +0000

    m

--- BA1A.py
+++ BA1A.py
@@ -1,1 +1,14 @@
-#BA1A
+# Compute the Number of Times a Pattern Appears in a Text
+
+def PatternCount(text, pattern):
+    count = 0
+    for i in range(len(text) - len(pattern)):
+        if text[i:i+len(pattern)-1] == pattern:
+            count += 1
+    return count
+
+text = "GCGCG"
+pattern = "GCG"
+
+result = PatternCount(text, pattern)
+print(result)
